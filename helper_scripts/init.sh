90199645392c75cad2c812be3b4a18dc78d4c7aa
anastasiya-lazar <leloneya@gmail.com>
2022-08-22 11:04:34 +0200

    Revert "[RD-12601] - Added models, migrations, dockerfile"
    This reverts commit 78907a82772d8d755b60e5eac1f19e0676206f58.

--- helper_scripts/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p .git/hooks || sudo -p mkdir .git/hooks
-cp -R ./.hooks/* ./.git/hooks/
-
-chmod +x ./.git/hooks/commit-msg
--- helper_scripts/migrate_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /usr/src/alembic
-
-alembic upgrade head
